6356443b0af0d3856e1566f5b12a4740b55fed5a
Rogério Terciotte <rterciotte@gmail.com>
2020-09-04 18:56:55 -0300

    Improving tests

--- spec/features/employee_publishes_an_ad_spec.rb
+++ spec/features/employee_publishes_an_ad_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 feature 'Employee publishes an ad' do
     scenario 'successfully' do
+        user  = User.create!(full_name: 'Rogério Terciotte', social_name: 'Rogério', 
+                            birth_date: '01/01/1983', occupation: 'Vendedor', department: 'Vendas', company_name: 'Campus Code',
+                            email: 'rogerio@email.com', password: '12345678')
         visit root_path
         login_as :user
+        expect(current_path).to eq root_path
         click_on 'Criar Anúncio'
         fill_in 'Nome', with: 'Guitarra'
         fill_in 'Categoria', with: 'Instrumento Musical'
--- spec/features/employee_register_account_spec.rb
+++ spec/features/employee_register_account_spec.rb
@@ -5,7 +5,7 @@ feature 'Employee register account' do
 
     visit root_path
  
-    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content('Escambo Net')
   end
 
   scenario 'and register account' do
@@ -23,5 +23,13 @@ feature 'Employee register account' do
     fill_in 'Setor', with: 'Sales'
     click_on 'Sign up'
     expect(page).to have_content('Editar perfil')
+    click_on 'Logout'
+    visit root_path
+    click_on 'Login'
+    fill_in 'Email', with: 'lorem@ipsum.com'
+    fill_in 'Senha', with: '123456'
+    
+    click_on 'Log in'
+    expect(page).to have_content('Login efetuado com sucesso!')
   end
 end
